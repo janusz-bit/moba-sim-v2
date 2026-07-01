0cdeb747fb3e9757e46b1d6070ea264afa4904a6
janusz-bit <janusz-bit@proton.me>
2026-07-01 11:42:22 +0200

    chore: add Nix flake devShell with clang toolchain

---
+++ nix/default.nix
@@ -0,0 +1,46 @@
+{ inputs, ... }:
+{
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
+  ];
+
+  imports = [
+    inputs.git-hooks-nix.flakeModule
+  ];
+
+  perSystem =
+    {
+      config,
+      pkgs,
+      ...
+    }:
+    {
+      formatter = pkgs.nixfmt-tree;
+      pre-commit.settings.hooks = {
+        nixfmt.enable = true;
+        clang-format.enable = true;
+        clang-tidy.enable = true;
+      };
+
+      devShells.default = pkgs.mkShell {
+        shellHook = ''
+          ${config.pre-commit.installationScript}
+        '';
+
+        packages =
+          config.pre-commit.settings.enabledPackages
+          ++ (with pkgs; [
+            # add packages to use in shell
+            cmake
+            ninja
+            clang
+            pkgsCross.mingwW64.buildPackages.gcc
+            wine64
+            clang-tools
+            lldb
+            boost
+          ]);
+      };
+    };
+}
