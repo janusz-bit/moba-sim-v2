fb21809c49c3389812e463769d8db6131ff64a3d
janusz-bit <janusz-bit@proton.me>
2026-07-04 12:26:59 +0200

    build: wire test_scenarios.cpp into moba_sim_tests

---
+++ tests/test_scenarios.cpp
@@ -0,0 +1,11 @@
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <cmath>
+
+#include "moba_sim.hpp"
+
+using moba::Champion;
+using moba::ModType;
+using moba::Source;
+using moba::Stat;
+using Stats = Champion::Stats;
